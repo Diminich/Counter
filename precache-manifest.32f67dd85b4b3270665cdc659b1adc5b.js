0c3a367eda748082642229d522682491465375df
Dima-wq <Hodarenokd@gmail.com>
2019-11-12 16:28:02 +0200

    Updates

---
+++ precache-manifest.32f67dd85b4b3270665cdc659b1adc5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc11862055e69d5c0fa7ec2ea179915",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "0ff99c9e36ecac964a93",
+    "url": "/Counter/static/css/main.f2a3e7ce.chunk.css"
+  },
+  {
+    "revision": "0eb8fe9d914e9b7a599d",
+    "url": "/Counter/static/js/2.0bbeceae.chunk.js"
+  },
+  {
+    "revision": "0ff99c9e36ecac964a93",
+    "url": "/Counter/static/js/main.1dd6fbf8.chunk.js"
+  },
+  {
+    "revision": "168bfd0b5b08746b3a01",
+    "url": "/Counter/static/js/runtime-main.d84aa68f.js"
+  }
+]);
